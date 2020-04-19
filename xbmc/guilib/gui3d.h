ffb94e7b1785ac58fe0422c0b9a4acf37bc25ed4
Rechi <Rechi@users.noreply.github.com>
2020-04-19 08:19:52 +0100

    [cleanup] remove unused symbols

--- xbmc/guilib/gui3d.h
+++ xbmc/guilib/gui3d.h
@@ -10,16 +10,6 @@
 
 #include "PlatformDefs.h" // for TARGET_WINDOWS types
 
-#define GAMMA_RAMP_FLAG  D3DSGR_CALIBRATE
-
-#define D3DFMT_LIN_A8R8G8B8 D3DFMT_A8R8G8B8
-#define D3DFMT_LIN_X8R8G8B8 D3DFMT_X8R8G8B8
-#define D3DFMT_LIN_L8       D3DFMT_L8
-#define D3DFMT_LIN_D16      D3DFMT_D16
-#define D3DFMT_LIN_A8       D3DFMT_A8
-
-#define D3DPIXELSHADERDEF DWORD
-
 struct D3DTexture
 {
   DWORD Common;
@@ -29,16 +19,3 @@ struct D3DTexture
   DWORD Format;   // Format information about the texture.
   DWORD Size;     // Size of a non power-of-2 texture, must be zero otherwise
 };
-
-#define D3DCOMMON_TYPE_MASK 0x0070000
-#define D3DCOMMON_TYPE_TEXTURE 0x0040000
-
-struct D3DPalette
-{
-  DWORD Common;
-  DWORD Data;
-  DWORD Lock;
-};
-
-typedef D3DPalette* LPDIRECT3DPALETTE8;
-
