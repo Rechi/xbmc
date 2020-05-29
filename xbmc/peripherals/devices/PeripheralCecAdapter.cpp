84d6f01575d9ed104c88a22570a328e7546d9f04
Rechi <Rechi@users.noreply.github.com>
2020-05-29 10:07:05 +0100

    [modernize][peripherals] google readability casting

--- xbmc/peripherals/devices/PeripheralCecAdapter.cpp
+++ xbmc/peripherals/devices/PeripheralCecAdapter.cpp
@@ -259,7 +259,7 @@ void CPeripheralCecAdapter::SetVersionInfo(const libcec_configuration &configura
   // append firmware build date
   if (configuration.iFirmwareBuildDate != CEC_FW_BUILD_UNKNOWN)
   {
-    CDateTime dt((time_t)configuration.iFirmwareBuildDate);
+    CDateTime dt(static_cast<time_t>(configuration.iFirmwareBuildDate));
     m_strVersionInfo += StringUtils::Format(" (%s)", dt.GetAsDBDate().c_str());
   }
 }
@@ -718,7 +718,7 @@ void CPeripheralCecAdapter::CecAlert(void *cbParam, const libcec_alert alert, co
   {
     std::string strLog(g_localizeStrings.Get(iAlertString));
     if (data.paramType == CEC_PARAMETER_TYPE_STRING && data.paramData)
-      strLog += StringUtils::Format(" - %s", (const char *)data.paramData);
+      strLog += StringUtils::Format(" - %s", static_cast<const char *>(data.paramData));
     CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Info, g_localizeStrings.Get(36000), strLog);
   }
 
@@ -1257,13 +1257,13 @@ void CPeripheralCecAdapter::SetConfigurationFromLibCEC(const CEC::libcec_configu
   bChanged |= SetSetting("activate_source", m_configuration.bActivateSource == 1);
 
   m_configuration.iDoubleTapTimeoutMs = config.iDoubleTapTimeoutMs;
-  bChanged |= SetSetting("double_tap_timeout_ms", (int)m_configuration.iDoubleTapTimeoutMs);
+  bChanged |= SetSetting("double_tap_timeout_ms", static_cast<int>(m_configuration.iDoubleTapTimeoutMs));
 
   m_configuration.iButtonRepeatRateMs = config.iButtonRepeatRateMs;
-  bChanged |= SetSetting("button_repeat_rate_ms", (int)m_configuration.iButtonRepeatRateMs);
+  bChanged |= SetSetting("button_repeat_rate_ms", static_cast<int>(m_configuration.iButtonRepeatRateMs));
 
   m_configuration.iButtonReleaseDelayMs = config.iButtonReleaseDelayMs;
-  bChanged |= SetSetting("button_release_delay_ms", (int)m_configuration.iButtonReleaseDelayMs);
+  bChanged |= SetSetting("button_release_delay_ms", static_cast<int>(m_configuration.iButtonReleaseDelayMs));
 
   m_configuration.bPowerOffOnStandby = config.bPowerOffOnStandby;
 
@@ -1289,11 +1289,11 @@ void CPeripheralCecAdapter::SetConfigurationFromSettings(void)
   // set the primary device type
   m_configuration.deviceTypes.Clear();
   int iDeviceType = GetSettingInt("device_type");
-  if (iDeviceType != (int)CEC_DEVICE_TYPE_RECORDING_DEVICE &&
-      iDeviceType != (int)CEC_DEVICE_TYPE_PLAYBACK_DEVICE &&
-      iDeviceType != (int)CEC_DEVICE_TYPE_TUNER)
-    iDeviceType = (int)CEC_DEVICE_TYPE_RECORDING_DEVICE;
-  m_configuration.deviceTypes.Add((cec_device_type)iDeviceType);
+  if (iDeviceType != static_cast<int>(CEC_DEVICE_TYPE_RECORDING_DEVICE) &&
+      iDeviceType != static_cast<int>(CEC_DEVICE_TYPE_PLAYBACK_DEVICE) &&
+      iDeviceType != static_cast<int>(CEC_DEVICE_TYPE_TUNER))
+    iDeviceType = static_cast<int>(CEC_DEVICE_TYPE_RECORDING_DEVICE);
+  m_configuration.deviceTypes.Add(static_cast<cec_device_type>(iDeviceType));
 
   // always try to autodetect the address.
   // when the firmware supports this, it will override the physical address, connected device and hdmi port settings
@@ -1382,7 +1382,7 @@ void CPeripheralCecAdapter::ReadLogicalAddresses(const std::string &strString, c
     {
       int iDevice(0);
       if (sscanf(strDevice.c_str(), "%x", &iDevice) == 1 && iDevice >= 0 && iDevice <= 0xF)
-        addresses.Set((cec_logical_address)iDevice);
+        addresses.Set(static_cast<cec_logical_address>(iDevice));
     }
   }
 }
