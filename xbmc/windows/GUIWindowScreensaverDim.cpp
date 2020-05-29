dfe46ce994f6201623c35110019e5da6579aa63b
Rechi <Rechi@users.noreply.github.com>
2020-05-29 10:07:05 +0100

    fixup! [modernize][windows] google readability casting

--- xbmc/windows/GUIWindowScreensaverDim.cpp
+++ xbmc/windows/GUIWindowScreensaverDim.cpp
@@ -57,7 +57,9 @@ void CGUIWindowScreensaverDim::Process(unsigned int currentTime, CDirtyRegionLis
   if (m_newDimLevel != m_dimLevel && !IsAnimating(ANIM_TYPE_WINDOW_CLOSE))
     m_dimLevel = m_newDimLevel;
   CGUIDialog::Process(currentTime, dirtyregions);
-  m_renderRegion.SetRect(0, 0, static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetWidth()), static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetHeight()));
+  m_renderRegion.SetRect(
+      0, 0, static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetWidth()),
+      static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetHeight()));
 }
 
 void CGUIWindowScreensaverDim::Render()
@@ -65,7 +67,8 @@ void CGUIWindowScreensaverDim::Render()
   // draw a translucent black quad - fading is handled by the window animation
   UTILS::Color color = (static_cast<UTILS::Color>(m_dimLevel * 2.55f) & 0xff) << 24;
   color = CServiceBroker::GetWinSystem()->GetGfxContext().MergeAlpha(color);
-  CRect rect(0, 0, static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetWidth()), static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetHeight()));
+  CRect rect(0, 0, static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetWidth()),
+             static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetHeight()));
   CGUITexture::DrawQuad(rect, color);
   CGUIDialog::Render();
 }
