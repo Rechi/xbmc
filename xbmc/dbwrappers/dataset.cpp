472b889592e2c670942823a4f9c6b9e8e48d9f10
Rechi <Rechi@users.noreply.github.com>
2023-12-06 12:27:39 +1000

    [clang-tidy][dbwrappers] google-readability-casting

--- xbmc/dbwrappers/dataset.cpp
+++ xbmc/dbwrappers/dataset.cpp
@@ -175,7 +175,7 @@ void Dataset::parse_sql(std::string& sql)
     while ((idx = sql.find(fpattern, next_idx)) >= 0)
     {
       next_idx = idx + fpattern.size();
-      if (sql.length() > ((unsigned int)next_idx))
+      if (sql.length() > (static_cast<unsigned int>(next_idx)))
         if (isalnum(sql[next_idx]) || sql[next_idx] == '_')
         {
           continue;
@@ -193,7 +193,7 @@ void Dataset::parse_sql(std::string& sql)
     while ((idx = sql.find(fpattern, next_idx)) >= 0)
     {
       next_idx = idx + fpattern.size();
-      if (sql.length() > ((unsigned int)next_idx))
+      if (sql.length() > (static_cast<unsigned int>(next_idx)))
         if (isalnum(sql[next_idx]) || sql[next_idx] == '_')
         {
           continue;
@@ -218,7 +218,7 @@ bool Dataset::seek(int pos)
   frecno = (pos < num_rows() - 1) ? pos : num_rows() - 1;
   frecno = (frecno < 0) ? 0 : frecno;
   fbof = feof = (num_rows() == 0) ? true : false;
-  return ((bool)frecno);
+  return (static_cast<bool>(frecno));
 }
 
 void Dataset::refresh()
@@ -415,7 +415,7 @@ const field_value& Dataset::get_field_value(int index)
 
 const sql_record* Dataset::get_sql_record()
 {
-  if (result.records.empty() || frecno >= (int)result.records.size())
+  if (result.records.empty() || frecno >= static_cast<int>(result.records.size()))
     return NULL;
 
   return result.records[frecno];
--- xbmc/dbwrappers/mysqldataset.cpp
+++ xbmc/dbwrappers/mysqldataset.cpp
@@ -788,11 +788,11 @@ char MysqlDatabase::et_getdigit(double* val, int* cnt)
   double d;
   if ((*cnt)++ >= 16)
     return '0';
-  digit = (int)*val;
+  digit = static_cast<int>(*val);
   d = digit;
   digit += '0';
   *val = (*val - d) * 10.0;
-  return (char)digit;
+  return static_cast<char>(digit);
 }
 
 /*
@@ -801,7 +801,7 @@ char MysqlDatabase::et_getdigit(double* val, int* cnt)
 void MysqlDatabase::appendSpace(StrAccum* pAccum, int N)
 {
   static const char zSpaces[] = "                             ";
-  while (N >= (int)sizeof(zSpaces) - 1)
+  while (N >= static_cast<int>(sizeof(zSpaces)) - 1)
   {
     mysqlStrAccumAppend(pAccum, zSpaces, sizeof(zSpaces) - 1);
     N -= sizeof(zSpaces) - 1;
@@ -1136,7 +1136,7 @@ void MysqlDatabase::mysqlVXPrintf(StrAccum* pAccum, /* Accumulate results here *
             longvalue = longvalue / base;
           } while (longvalue > 0);
         }
-        length = (int)(&buf[etBUFSIZE - 1] - bufpt);
+        length = static_cast<int>(&buf[etBUFSIZE - 1] - bufpt);
         for (idx = precision - length; idx > 0; idx--)
         {
           *(--bufpt) = '0'; /* Zero pad */
@@ -1151,7 +1151,7 @@ void MysqlDatabase::mysqlVXPrintf(StrAccum* pAccum, /* Accumulate results here *
           for (; (x = (*pre)) != 0; pre++)
             *(--bufpt) = x;
         }
-        length = (int)(&buf[etBUFSIZE - 1] - bufpt);
+        length = static_cast<int>(&buf[etBUFSIZE - 1] - bufpt);
         bufpt[length] = 0;
         break;
       case etFLOAT:
@@ -1347,18 +1347,18 @@ void MysqlDatabase::mysqlVXPrintf(StrAccum* pAccum, /* Accumulate results here *
           }
           if (exp >= 100)
           {
-            *(bufpt++) = (char)((exp / 100) + '0'); /* 100's digit */
+            *(bufpt++) = static_cast<char>((exp / 100) + '0'); /* 100's digit */
             exp %= 100;
           }
-          *(bufpt++) = (char)(exp / 10 + '0'); /* 10's digit */
-          *(bufpt++) = (char)(exp % 10 + '0'); /* 1's digit */
+          *(bufpt++) = static_cast<char>(exp / 10 + '0'); /* 10's digit */
+          *(bufpt++) = static_cast<char>(exp % 10 + '0'); /* 1's digit */
         }
         *bufpt = 0;
 
         /* The converted number is in buf[] and zero terminated. Output it.
         ** Note that the number is in the usual order, not reversed as with
         ** integer conversions. */
-        length = (int)(bufpt - buf);
+        length = static_cast<int>(bufpt - buf);
         bufpt = buf;
 
         /* Special case:  Add leading zeros if the flag_zeropad flag is
@@ -1388,11 +1388,11 @@ void MysqlDatabase::mysqlVXPrintf(StrAccum* pAccum, /* Accumulate results here *
         break;
       case etCHARX:
         c = va_arg(ap, int);
-        buf[0] = (char)c;
+        buf[0] = static_cast<char>(c);
         if (precision >= 0)
         {
           for (idx = 1; idx < precision; idx++)
-            buf[idx] = (char)c;
+            buf[idx] = static_cast<char>(c);
           length = precision;
         }
         else
@@ -1446,7 +1446,7 @@ void MysqlDatabase::mysqlVXPrintf(StrAccum* pAccum, /* Accumulate results here *
         n = i * 2 + 1 + needQuote * 2;
         if (n > etBUFSIZE)
         {
-          bufpt = zExtra = (char*)malloc(n);
+          bufpt = zExtra = static_cast<char*>(malloc(n));
           if (bufpt == 0)
           {
             pAccum->mallocFailed = true;
@@ -1542,7 +1542,7 @@ bool MysqlDatabase::mysqlStrAccumAppend(StrAccum* p, const char* z, int N)
     {
       p->nAlloc = szNew;
     }
-    zNew = (char*)malloc(p->nAlloc);
+    zNew = static_cast<char*>(malloc(p->nAlloc));
     if (zNew)
     {
       memcpy(zNew, p->zText, p->nChar);
@@ -1584,7 +1584,7 @@ char* MysqlDatabase::mysqlStrAccumFinish(StrAccum* p)
     p->zText[p->nChar] = 0;
     if (p->zText == p->zBase)
     {
-      p->zText = (char*)malloc(p->nChar + 1);
+      p->zText = static_cast<char*>(malloc(p->nChar + 1));
       if (p->zText)
       {
         memcpy(p->zText, p->zBase, p->nChar + 1);
@@ -1729,7 +1729,7 @@ void MysqlDataset::make_deletion()
 void MysqlDataset::fill_fields()
 {
   if ((db == NULL) || (result.record_header.empty()) ||
-      (result.records.size() < (unsigned int)frecno))
+      (result.records.size() < static_cast<unsigned int>(frecno)))
     return;
 
   if (fields_object->size() == 0) // Filling columns name
@@ -2053,7 +2053,7 @@ void MysqlDataset::next(void)
 
 void MysqlDataset::free_row(void)
 {
-  if (frecno < 0 || (unsigned int)frecno >= result.records.size())
+  if (frecno < 0 || static_cast<unsigned int>(frecno) >= result.records.size())
     return;
 
   sql_record* row = result.records[frecno];
