821c39d08e2cf1807bec048478bb4a4e3ab5e5a5
Rechi <Rechi@users.noreply.github.com>
2024-01-21 21:30:46 -0800

    [clang-tidy][paplayer] modernize-loop-convert

--- xbmc/cores/paplayer/PAPlayer.cpp
+++ xbmc/cores/paplayer/PAPlayer.cpp
@@ -58,9 +58,8 @@ PAPlayer::~PAPlayer()
 void PAPlayer::SoftStart(bool wait/* = false */)
 {
   std::unique_lock<CCriticalSection> lock(m_streamsLock);
-  for(StreamList::iterator itt = m_streams.begin(); itt != m_streams.end(); ++itt)
+  for (const StreamInfo* si : m_streams)
   {
-    StreamInfo* si = *itt;
     if (si->m_fadeOutTriggered)
       continue;
 
@@ -79,9 +78,8 @@ void PAPlayer::SoftStart(bool wait/* = false */)
     while(wait)
     {
       wait = false;
-      for(StreamList::iterator itt = m_streams.begin(); itt != m_streams.end(); ++itt)
+      for (const StreamInfo* si : m_streams)
       {
-        StreamInfo* si = *itt;
         if (si->m_stream->IsFading())
         {
           lock.unlock();
@@ -99,9 +97,8 @@ void PAPlayer::SoftStop(bool wait/* = false */, bool close/* = true */)
 {
   /* fade all the streams out fast for a nice soft stop */
   std::unique_lock<CCriticalSection> lock(m_streamsLock);
-  for(StreamList::iterator itt = m_streams.begin(); itt != m_streams.end(); ++itt)
+  for (StreamInfo* si : m_streams)
   {
-    StreamInfo* si = *itt;
     if (si->m_stream)
       si->m_stream->FadeVolume(1.0f, 0.0f, FAST_XFADE_TIME);
 
@@ -128,9 +125,8 @@ void PAPlayer::SoftStop(bool wait/* = false */, bool close/* = true */)
     while(wait && !CServiceBroker::GetActiveAE()->IsSuspended() && !timer.IsTimePast())
     {
       wait = false;
-      for(StreamList::iterator itt = m_streams.begin(); itt != m_streams.end(); ++itt)
+      for (const StreamInfo* si : m_streams)
       {
-        StreamInfo* si = *itt;
         if (si->m_stream && si->m_stream->IsFading())
         {
           lock.unlock();
@@ -145,9 +141,8 @@ void PAPlayer::SoftStop(bool wait/* = false */, bool close/* = true */)
     /* if we are not closing the streams, pause them */
     if (!close)
     {
-      for(StreamList::iterator itt = m_streams.begin(); itt != m_streams.end(); ++itt)
+      for (const StreamInfo* si : m_streams)
       {
-        StreamInfo* si = *itt;
         si->m_stream->Pause();
       }
     }
