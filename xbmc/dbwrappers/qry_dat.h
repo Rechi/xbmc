735f7518d2f0c5a2722263a3ac9f3d273cdcdaee
Rechi <Rechi@users.noreply.github.com>
2024-01-21 21:30:46 -0800

    [clang-tidy][dbwrappers] modernize-loop-convert

--- xbmc/dbwrappers/qry_dat.h
+++ xbmc/dbwrappers/qry_dat.h
@@ -259,9 +259,9 @@ public:
   ~result_set() { clear(); };
   void clear()
   {
-    for (unsigned int i = 0; i < records.size(); i++)
-      if (records[i])
-        delete records[i];
+    for (const sql_record* record : records)
+      if (record)
+        delete record;
     records.clear();
     record_header.clear();
   };
